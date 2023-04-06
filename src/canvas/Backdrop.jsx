efd091d2521699ce5691b6a0bb80d45466846134
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-06 07:55:07 -0300

    Backdrop config

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {useRef} from 'react';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows
+      position={0}
+    >
+      <RandomizedLight
+        amount={4}
+      />
+    </AccumulativeShadows>
   )
 }
 
